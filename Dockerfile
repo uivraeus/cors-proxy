44a6db2a14876598b850a45e75b48854ba915313
ulf <uivraeus@gmail.com>
2022-06-06 17:15:56 +0200

    Major rewrite with fewer dependencies
    Avoid express and the deprecated `request` package. Use Node (18) native
    `https` instead.
    
    For now, there is no support for rate-limitation.

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:16-alpine
+FROM node:18-alpine
 
 COPY ./package.json /cors-prox/
 COPY ./package-lock.json /cors-prox/
--- src/app.mjs
+++ src/app.mjs
@@ -1,145 +1,205 @@
-import express from 'express';
-import request from 'request';
+import { createServer } from 'http'
+import { get } from 'https'
 import validUrl from 'valid-url';
-import rateLimit from 'express-rate-limit';
+
+//Don't hang for more than 3s if target can't be reached
 
 //Handle configuration
-const port = process.env.PROXY_PORT || 3000;
-const apiRoute = process.env.PROXY_ROUTE || "/cors-proxy/";
-const rateLimitWindowMs =  process.env.PROXY_RL_WINDOW_MS || (5 * 60 * 1000); //5 minutes
-const rateLimitMax = process.env.PROXY_RL_WINDOW_MS || 20; //max #requests per windows (per IP)
+const PORT = Number(process.env.PROXY_PORT || 33000)
+const API_ROUTE = process.env.PROXY_ROUTE || "/cors-proxy/"
+const FWD_TIMEOUT_MS =  Number(process.env.PROXY_TIMEOUT_MS || 3000)
 
-//If not configured, then allow all. But if misconfigured, then bail out
-const allowOrigins = process.env.PROXY_ALLOW_ORIGINS
-  ? function parseOrigins() {
-      try {
-        const a = JSON.parse(process.env.PROXY_ALLOW_ORIGINS);
-        if (!Array.isArray(a)) throw new Error("Wrong type.");
-        return a;
-      } catch(e) {
-        console.error("PROXY_ALLOW_ORIGINS not defined as JSON array:\n", e.message);
-        process.exit(1);
-      }
-    }()
-  : [];
+//Assertive parsing of environment variable which must be a (json) array
+const allowOrigins = parseEnvArray("PROXY_ALLOW_ORIGINS")
+const allowedTargetPatterns = parseEnvArray("PROXY_ALLOW_TARGET_PATTERNS")
 
-const allowedTargetPatterns = process.env.PROXY_ALLOW_TARGET_PATTERNS
-  ? function parseTargetPatterns() {
-      try {
-        const a = JSON.parse(process.env.PROXY_ALLOW_TARGET_PATTERNS);
-        if (!Array.isArray(a)) throw new Error("Wrong type.");
-        return a;
-      } catch(e) {
-        console.error("PROXY_ALLOW_TARGET_PATTERNS not defined as JSON array:\n", e.message);
-        process.exit(1);
-      }
-    }()
-  : [];
+//Limit which headers that will be forwarded (back and forth)
+const includedReqHeaders = [
+  "accept",
+  "user-agent"
+]
+const includedResHeaders = [
+  "content-type",
+  "cache-control",
+  "last-modified",
+  "content-length"
+]
 
-//Initialize the (Express) app
-const app = express()
-app.disable('x-powered-by');
+//Initialize and start the proxy server
+const options = {}
+const server = createServer(options)
+server.on("request", serveClientReq)
 
-//Make rate-limiter work behind a reverse proxy
-app.set('trust proxy', 1);
-const limiter = rateLimit({
-  windowMs: rateLimitWindowMs,
-  max: rateLimitMax
-});
-app.use(limiter);
+server.listen(PORT)
+console.log(`${utc()}: Server listening on port: ${PORT}`)
 
-app.all(`${apiRoute}*`, function (req, res, next) {
-  // For logging
-  const ip = req.ip;
-  const ipFW = req.headers['x-forwarded-for'] || req.socket.remoteAddress 
-
-  const origin = req.header('origin');
-  if (!origin ||isAllowedOrigin(origin)) {
-    if (origin) {
-      // Set CORS headers:
-      res.header("Access-Control-Allow-Origin", origin);
-      res.header("Access-Control-Allow-Methods", "GET");
-      res.header("Access-Control-Allow-Headers", req.header('access-control-request-headers'));
-    }
+/* Common request listener 
+ */
+function serveClientReq(clientReq, clientRes) {
+  const clientIp = clientReq.headers['x-forwarded-for'] || clientReq.socket.remoteAddress 
+  const origin = clientReq.headers['origin']
   
-    try {
-      if (req.method === 'OPTIONS') {
-        // CORS Preflight
-        console.log(`${new Date().toISOString()}: OPTIONS / Preflight`);
-        res.send();
-      } else if (req.method === 'GET') {
-        const target = encodeURI(req.params[0]);
-        if (!target || !validUrl.isHttpsUri(target) || !isAllowedTarget(target)) {
-          const msg = "There is no valid Target-Endpoint in the request";
-          console.log(`${new Date().toISOString()}: ${msg}`, target);
-          res.status(400).send(msg);
-        } else {
-          const reqType = origin ? "COORS" : "back-end API";
-          console.log(`${new Date().toISOString()}: Try proxy ${reqType} request from ${ip}/${ipFW} to ${target}`);
+  // Early exit if origin is not allowed
+  if (origin && !isAllowedOrigin(origin)) {
+    console.log(`${utc()}: Discard proxy request from ${clientIp}, origin ${origin} not allowed`);
+    resEnd(clientRes, 406)
+    return
+  }
 
-          request({ url: target, method: req.method, headers: {'accept': req.header('accept')}})
-          .on('error', err => {
-            const msg = "Can't connect to target.";
-            console.error(msg, err);
-            res.status(500).send(msg);
-          })
-          .on('response', resp => {
-            console.log("Done:", resp.statusCode, resp.statusMessage, resp.headers);
-          })
-          .pipe(res);
-        }
-      } else {
-          console.log(`${new Date().toISOString()}: Not GET/OPTIONS`, req.method, JSON.stringify(req.headers));
-          res.set('Allow', 'GET', 'OPTIONS');
-          res.send(405, 'Method Not Allowed');      
+  // Is this a valid API request? 
+  const fullUrl = clientReq.url
+  if (fullUrl.startsWith(API_ROUTE)) {
+    const target = fullUrl.slice(API_ROUTE.length)
+
+    // Early exit if the target isn't within specs
+    if (!target || !validUrl.isHttpsUri(target) || !isAllowedTarget(target)) {
+      console.log(`${utc()}: There is no valid target endpoint in the request:`, target || "<none>");
+      resEnd(clientRes, 400)
+      return
+    }
+
+    const method = clientReq.method
+    if (method === "OPTIONS" || method === "GET") {
+      if (origin) {
+        setCoorsHeaders(clientReq, clientRes, origin)
       }
-    } catch (error) {
-      const msg = "Can't connect to target.";
-      console.error(msg,error.message);
-      res.status(500).end();
+      if (method === "OPTIONS") {
+        // CORS Preflight
+        resEnd(200)
+      } else if (method === "GET") {
+        const reqType = origin ? "COORS" : "back-end API";
+        console.log(`${utc()}: Try proxy ${reqType} request from ${clientIp} to ${target}`);
+        fwdGetRequest(clientReq, target, clientRes);
+      }
+    } else {
+      console.log(`${utc()}: Unsupported method in request from ${clientIp}: ${clientReq.method}`);
+      resEnd(clientRes, 405)
     }
   } else {
-    const msg = origin ? `Origin ${origin} not allowed` : "Origin not provided";
-    console.log(`${new Date().toISOString()}: Discard proxy request from ${ip}/${ipFW}`);
-    console.error(msg);
-    res.status(406).end();
-  }  
-});
-
-app.listen(port, () => {
-  console.log(`${new Date().toISOString()}: CORS-proxy running with configuration:`);
-  console.log(` port/route            : ${port}${apiRoute}`);
-  console.log(` rate-limit window/max : ${rateLimitWindowMs}/${rateLimitMax}`);
-  console.log(` allow origins         : [${allowOrigins}]`);
-  console.log(` allow targets         : [${allowedTargetPatterns}]`);
-})
-
-//Helper for deriving the applicable Allow-origin (or null if actually not allowed)
-const isAllowedOrigin = (origin) => {
-  //Incorrect request -> deny
-  if (!origin) return false;
-
-  //Valid request, when we haven't limited usage
-  if (allowOrigins.length === 0) return true;
-
-  //Otherwise, check if origin included in the list
-  if (allowOrigins.findIndex(o => o === origin) !== -1) {
-    return true;
-  } else {
-    return false;
+    console.log(`${utc()}: Unsupported URL in request from ${clientIp}: ${fullUrl}`);
+    resEnd(clientRes, 404)
   }
 }
 
-const isAllowedTarget = (target) => {
+function fwdGetRequest(clientReq, target, clientRes) {
+  const targetOpt = {
+    headers: copyHeaders(includedReqHeaders, clientReq.headers),
+    timeout: FWD_TIMEOUT_MS
+  };
+  const targetReq = get(target, targetOpt, targetRes => {
+    console.log(`${utc()}: -> status code: ${targetRes.statusCode}`);
+
+    if (targetRes.statusCode !== 200) {
+      targetRes.resume(); // Consume response data to free up memory
+      resEnd(clientRes, targetRes.statusCode)
+      return; //no need to configure event handlers
+    }    
+  
+    // Forward a few selected headers (if present)
+    includedResHeaders.forEach(name => fwdHeader(clientRes, name, targetRes.headers));
+    
+    // Configure handlers for managing the actual data forwarding
+    targetRes.on("data", chunk => clientRes.write(chunk));
+    targetRes.on("end", () => clientRes.end());
+    targetRes.on("error", e => {
+      console.error(`${utc()}: ERROR: failed to GET from target, error:`, e.message || e);
+      // Is is feasible to set the status code here? What if error occurs half way through the chunks?
+      resEnd(clientRes)
+    });
+  });
+  
+  //Specific error instance to distinguish special timeout case
+  const timeoutError = new Error("target GET timeout")
+
+  targetReq.on("error", e => {
+    console.error(`${utc()}: ERROR: failed to GET from target, error:`, e.message || e);
+    resEnd(clientRes, e === timeoutError ? 504 : 500)
+  });
+  targetReq.on("timeout", () => {
+    targetReq.destroy(timeoutError);
+  });
+}
+
+function fwdHeader(res, name, srcHeaders) {
+  const value = srcHeaders[name]
+  if (value !== undefined) {
+    res.setHeader(name, value)
+  }
+}
+
+function copyHeaders(headerNames, srcHeaders) {
+  let dstHeaders = {}
+  headerNames.forEach(name => {
+    const value = srcHeaders[name]
+    if (value !== undefined) {
+      dstHeaders[name] = srcHeaders[name]
+    }
+  });
+  return dstHeaders
+}
+
+function setCoorsHeaders(req, res, origin) {
+  res.setHeader("Access-Control-Allow-Origin", origin);
+  res.setHeader("Access-Control-Allow-Methods", "GET");
+  const acrh = req.headers['access-control-request-headers']
+  if (acrh) {
+    res.setHeader("Access-Control-Allow-Headers", acrh);
+  }
+}
+function resEnd(res, code) {
+  if (Number.isInteger(code)) {
+    res.writeHead(code)
+  }
+  res.end()
+}
+//Helper for deriving the applicable Allow-origin (or null if actually not allowed)
+function isAllowedOrigin (origin) {
   //Incorrect request -> deny
-  if (!target) return false;
+  if (!origin) return false
+
+  //Valid request, when we haven't limited usage
+  if (allowOrigins.length === 0) return true
+
+  //Otherwise, check if origin included in the list
+  if (allowOrigins.findIndex(o => o === origin) !== -1) {
+    return true
+  } else {
+    return false
+  }
+}
+
+function isAllowedTarget(target) {
+  //Incorrect request -> deny
+  if (!target) return false
   
   //Valid request, when we haven't limited usage
-  if (allowedTargetPatterns.length === 0) return true;
+  if (allowedTargetPatterns.length === 0) return true
 
   if (allowedTargetPatterns.findIndex(t => RegExp(t).test(target)) !== -1) {
-    return true;
+    return true
   } else {
-    return false;
+    return false
   }
+}
+
+//If not configured, then allow all. But if misconfigured, then bail out
+function parseEnvArray(varName) {
+  if (!process.env[varName]) {
+    return []
+  }
+  let raw = ""
+  try {
+    raw = process.env[varName]
+    const a = JSON.parse(raw)
+    if (!Array.isArray(a)) throw new Error("Wrong type.")
+    return a
+  } catch(e) {
+    console.error(varName, "not defined as JSON array:\n", e.message)
+    console.log("Raw value:", raw)
+    process.exit(1)
+  }
+}
+
+function utc() {
+  return new Date().toISOString()
 }
